3e1d1d5167e4e764bb89aba9fe9072655e0cb1ed
Samdoes <111439@st-maartenscollege.nl>
2024-11-05 23:12:19 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,16 @@
 var Bommenlijst = new Array("jan","jaap","robert","juliana")
-let aantalLevens = 2;
+let aantalLevens = 1;
+
+let oranjeRij = 6;
+let oranjeKolom = 9;
+let standaardAchtergrond;
+let oranjeAchtergrond;
+
+let appelGegetenGroen = false;
+let appelGegetenRood = false;
+
+let tijdOpRood = 0;
+
 
 var xPositie;
 var yPositie;
@@ -12,7 +23,7 @@ class Bom {
     this.x = x;
     this.y = y;
     this.sprite = null;
-    this.stapgrootte = floor(random(-3,4));
+    this.stapGrootte = floor(random(-3,4));
   }
   
   beweeg() {
@@ -33,16 +44,21 @@ class Apple {
     this.xSpeed = 1;
     this.ySpeed = 1;
     this.sprite = null;
+    this.active = true;
   }
   
   toon(){
-   image(this.sprite, this.xPositie, this.yPositie, raster.celGrootte, raster.celGrootte);
+    if(this.active){
+       image(this.sprite, this.xPositie, this.yPositie, raster.celGrootte, raster.celGrootte);
+    }
+  
   }
   
   beweeg() {
+    if(this.active){
   this.xPositie += this.xSpeed * raster.celGrootte;
   this.yPositie += this.ySpeed * raster.celGrootte;
-  
+  }
   
   if (this.xPositie <= 0 || this.xPositie >= canvas.width - raster.celGrootte) {
     this.xSpeed *= -1;  
@@ -58,7 +74,6 @@ class Apple {
   
 }
 
-
 class Raster {
   constructor(r,k) {
     this.aantalRijen = r;
@@ -66,7 +81,6 @@ class Raster {
     this.celGrootte = null;
   }
   
-
   
   berekenCelGrootte() {
     this.celGrootte = canvas.width / this.aantalKolommen;
@@ -76,9 +90,15 @@ class Raster {
     push();
     noFill();
     stroke('grey');
-    for (var rij = 0;rij < this.aantalRijen;rij++) {
-      for (var kolom = 0;kolom < this.aantalKolommen;kolom++) {
-        rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
+    for (let rij = 0; rij < this.aantalRijen; rij++) {
+      for (let kolom = 0; kolom < this.aantalKolommen; kolom++) {
+        if (rij === oranjeRij || kolom === oranjeKolom) {
+          fill('orange'); 
+        } else {
+          noFill(); 
+        }
+        rect(kolom * this.celGrootte, rij * this.celGrootte, this.celGrootte, this.celGrootte);
+        noFill(); 
       }
     }
     pop();
@@ -91,7 +111,7 @@ class Jos {
     this.y = 300;
     this.animatie = [];
     this.frameNummer =  3;
-    this.stapGrootte = null;
+    this.stapGrootte = raster.celGrootte;
     this.gehaald = false;
   }
   
@@ -120,31 +140,8 @@ class Jos {
       this.gehaald = true;
     }
   }
-  
-  wordtGeraakt(vijand) {
-    if (this.x == vijand.x && this.y == vijand.y) {
-      background('red');
-      return true;
-    }
-    else {
-      return false;
-    } 
     
-
-  }
-  
-  wordtGeraakt(bom) {
-    if (this.x == bom.x && this.y == bom.y) {
-      background('red');
-      return true;
-      
-    }
-    else {
-      return false;
-    }
-    
-    wordtGeraakt(groen) {
-    // Controleer of Eve de appel raakt
+   wordtGeraaktObject(object) {
     if (this.x < object.x + raster.celGrootte && 
         this.x + raster.celGrootte > object.x && 
         this.y < object.y + raster.celGrootte && 
@@ -153,8 +150,19 @@ class Jos {
     }
     return false;
 }
-    
-  }
+  
+  wordtGeraaktApple(apple) {
+    if (this.x < apple.xPositie + raster.celGrootte && 
+        this.x + raster.celGrootte > apple.xPositie && 
+        this.y < apple.yPositie + raster.celGrootte && 
+        this.y + raster.celGrootte > apple.yPositie) {
+        return true;
+    }
+    return false;
+}
+
+
+
   
   toon() {
     image(this.animatie[this.frameNummer],this.x,this.y,raster.celGrootte,raster.celGrootte);
@@ -166,7 +174,7 @@ class Vijand {
     this.x = x;
     this.y = y;
     this.sprite = null;
-    this.stapGrootte = null;
+    this.stapGrootte = raster.celGrootte;
   }
 
   beweeg() {
@@ -183,7 +191,8 @@ class Vijand {
 }
 
 function preload() {
-  brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
+  standaardAchtergrond = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
+  oranjeAchtergrond = loadImage("images/backgrounds/karton.jpg")
 }
 
 function setup() {
@@ -237,6 +246,11 @@ function setup() {
   groen.xSpeed = 1; 
   groen.ySpeed = 1; 
   groen.sprite = loadImage("images/sprites/appel_1.png");
+  
+  rood = new Apple(raster.celGrootte*2,raster.celGrootte*2);
+  rood.xSpeed = 1; 
+  rood.ySpeed = 1; 
+  rood.sprite = loadImage("images/sprites/appel_2.png");
 
   
  
@@ -244,7 +258,23 @@ function setup() {
 
 
 function draw() {
-  background(brug);
+  let celX = Math.floor(mouseX / raster.celGrootte);
+  let celY = Math.floor(mouseY / raster.celGrootte);
+  
+  if (celY === oranjeRij || celX === oranjeKolom) {
+    background(oranjeAchtergrond); 
+  } else {
+    background(standaardAchtergrond); 
+  }
+  
+  if (tijdOpRood > 0) {
+  background('red');
+  fill('white');
+  textSize(32);
+  text("Je bent geraakt!", 50, 300);
+  tijdOpRood--;
+  return;
+  }
   raster.teken();
   eve.beweeg();
   alice.beweeg();
@@ -255,6 +285,7 @@ function draw() {
   juliana.beweeg();
   bart.beweeg();
   groen.beweeg();
+  
   eve.toon();
   alice.toon();
   bob.toon();
@@ -263,16 +294,31 @@ function draw() {
   robert.toon();
   juliana.toon();
   bart.toon();
+  if(groen.active){
   groen.toon();
+  }
+  if (rood.active) {
+  rood.toon();
+}
 
 
-  if (eve.wordtGeraakt(jan) || eve.wordtGeraakt(jaap) || eve.wordtGeraakt(robert) || eve.wordtGeraakt(juliana) || eve.wordtGeraakt(bart) || eve.wordtGeraakt(alice) || eve.wordtGeraakt(bob)) {
+
+  if (eve.wordtGeraaktObject(jan) || eve.wordtGeraaktObject(jaap) || eve.wordtGeraaktObject(robert) || eve.wordtGeraaktObject(juliana) || eve.wordtGeraaktObject(bart) || eve.wordtGeraaktObject(alice) || eve.wordtGeraaktObject(bob)) {
     aantalLevens--;
+    tijdOpRood = 15;
   }
   
-  if (eve.wordtGeraakt(groen)){
-    aantalLevens ++;
-  }
+  if (eve.wordtGeraaktApple(rood) && rood.active) {
+  aantalLevens++;
+  rood.active = false;  // Deactivate the apple when eaten
+}
+  if (eve.wordtGeraaktApple(groen) && groen.active) {
+  aantalLevens++;
+  groen.active = false;  
+}
+  
+  
+
   
   if (eve.gehaald) {
     background('green');
